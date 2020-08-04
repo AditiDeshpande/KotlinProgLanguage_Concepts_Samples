0bed1b4162d5cd09d83691f0953e061cc9ba9633
aditi <aditid11@gmail.com>
2020-08-04 20:07:16 +0530

    Function parameters , default params

--- functionProg2.kts
+++ functionProg2.kts
@@ -54,3 +54,37 @@ fun sub(int1: Int , int2:Int) = int1 - int2
 
 println("Subtraction of 9 and 10 " +
         "is " +sub(9,10))
+
+fun multiply(a: Int , b: Int , c: Int=2)
+= a * b * c
+
+//All these println are / should be in main
+//when u write kotlin file instead of scratch
+//file
+println("Multiplication of 3 , 4 ,5 is " +
+         multiply(3,4,5))
+
+println("Multiplication of 3 , 4 is " +
+        multiply(3,4))
+
+
+fun printDetails(name: String , email: String = "" ,
+phone: String){
+    println("Name: $name - email: $email" +
+            "- phone: $phone")
+}
+//Foll. is giving me error no value passed for phone
+//so second value is taken for email
+//but as there is assigned "" default value for email
+//we wanted to pass second parameter as phone
+//but compiler didn't understand it that we wanted
+//to pass specific parameter let's see how we can
+//do it
+
+//printDetails("Aditi" , "95669-34556")
+
+//Foll won't give an error
+printDetails("Aditi" , phone = "9566 3455")
+
+printDetails(email = "aditid11" , phone = "123 456" ,
+name = "Aditi")
