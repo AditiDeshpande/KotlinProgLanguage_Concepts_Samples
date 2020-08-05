479b188772d72cb903b5f3289aa31f363def392c
aditi <aditid11@gmail.com>
2020-08-05 16:43:29 +0530

    Classes in Kotlin.

---
+++ Classes.kts
@@ -0,0 +1,47 @@
+class DailyActivity2{
+    //Foll. are not fields but properties
+    var actId = 0
+    var actName: String = ""
+
+    //Foll. is immutable property
+    //Duration/timer for activity everyday
+    //1 hour everyday and it's immutable
+    val timer = 01.00
+}
+
+//instance of class
+//note no "new" keyword in kotlin
+val activity = DailyActivity2()
+
+activity.actId = 11
+activity.actName = "Singing"
+
+
+//Different ways to initialize the params
+//specially not before passing from main
+//or not before class's object creation
+
+class DailyActivity1(initId: Int , initName: String){
+    //Foll lines giving error property must
+    //be initialized or abstract
+
+    /*var actId1: Int
+    var actName: String*/
+
+    var actId = initId
+    var actName = initName
+    val timer = 01.00
+}
+
+//no need to have body / curly braces too for
+//foll. type of class declaration
+class DailyActivity(var actId: Int , var actName: String = ""){
+    init {
+        actName = actName.toUpperCase()
+    }
+}
+
+var dailyActivity = DailyActivity(25 , "Painting")
+
+println("Activity Id: ${dailyActivity.actId}" +
+        "\nActivity Name: ${dailyActivity.actName}")
