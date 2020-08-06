1037c3d8816c9ea84b6598f98407737f3266bac9
aditi <aditid11@gmail.com>
2020-08-06 11:07:26 +0530

    Classes in Kotlin. Visibility Modifiers theory.

---
+++ VisibilityModifiers.kts
@@ -0,0 +1,18 @@
+/*
+
+Module is intellij , gradle , maven etc modules
+, here
+
+4 Visibility Modifiers
+public: Default and anywhere accessible
+
+Top level declarations
+private: Available inside file containing declaration
+internal: Anywhere in the same module
+
+Classes
+private: Only available to class members
+protected: same as private and subclasses
+internal: Any client inside the module
+ */
+
