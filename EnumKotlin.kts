c5cad8006653039c087005f078773abe55e00a8c
aditi <aditid11@gmail.com>
2020-08-07 18:49:42 +0530

    Enum in kotlin.

--- EnumKotlin.kts
+++ EnumKotlin.kts
@@ -1,10 +1,48 @@
-enum class CompletionStatus{
-    WIP ,
-    RESUMED ,
-    PARTIALLY_COMPLETE ,
-    COMPLETE
+enum class CompletionStatus(val value: Int) {
+    WIP(-1) ,
+    RESUMED(0) ,
+    PARTIALLY_COMPLETE(1) ,
+    COMPLETE(10)
 }
 
 val status = CompletionStatus.PARTIALLY_COMPLETE
 
 println("Status of Activity1 is $status")
+println("Status Value is $status.value")
+println("Ordinal Value is ${status.ordinal}")
+println("Value of status Completed is ${CompletionStatus.COMPLETE.value}")
+println("Ordinal Value of status Completed is ${CompletionStatus.COMPLETE.ordinal}")
+println("Name status Resumed is ${CompletionStatus.RESUMED.name}")
+
+
+
+/*
+Output
+Status of Activity1 is PARTIALLY_COMPLETE
+Status Value is PARTIALLY_COMPLETE.value
+Ordinal Value is 2
+Value of status Completed is 10
+Ordinal Value of status Completed is 3
+Name status Resumed is RESUMED
+ */
+
+for (statusInList in CompletionStatus.values()){
+println(statusInList)
+}
+
+/*
+Output
+WIP
+RESUMED
+PARTIALLY_COMPLETE
+COMPLETE
+ */
+
+println("Ordinal of status WIP is " +
+        CompletionStatus.valueOf("WIP").ordinal)
+
+/*
+Output
+Ordinal of status WIP is 0
+ */
+
